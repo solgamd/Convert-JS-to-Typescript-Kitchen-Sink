f4e5ca2e9e00bff759a57c9f6c8b390f40fc9fa5
solgamd <solgamd@gmail.com>
2019-06-18 21:09:01 +0200

    Fixed error by adding interface. Lab finished.

--- app.js
+++ app.js
@@ -1,52 +1,42 @@
+"use strict";
 //My name
-var firstName: string = "Megan";
-
+var firstName = "Megan";
 //Number of states in USA
-const states: number = 52;
-
+var states = 52;
 //Add 5 and 4
-var add: number = 5 + 4;
-
+var add = 5 + 4;
 //When called, executes an alert in a pop-up.
-function sayHello(): void {
+function sayHello() {
     alert("Hello World!");
-};
+}
+;
 sayHello();
-
 //When called, checks whether user is 21+, gives alert.
-function checkAge(firstName: string, age: number) {
+function checkAge(firstName, age) {
     if (age < 21) {
-        alert("Sorry " + firstName + ", you aren't old enough to view this page!")
+        alert("Sorry " + firstName + ", you aren't old enough to view this page!");
     }
-};
-
+}
+;
 //Calling function checkAge with parameters of people.
 checkAge("Charles", 21);
 checkAge("Abby", 27);
 checkAge("James", 18);
 checkAge("John", 17);
-
 //Declaring var of favorite veggies.
-var favveg: string[] = ["squash", "carrot", "peas"];
-
+var favveg = ["squash", "carrot", "peas"];
 //Calling list of veggies one by one.
 // for(var i = 0; i < favveg.length; i++) {
 //     console.log(favveg[i]);
 // }
-
 // for(let veg of favveg) {
 //     console.log(veg);
 // }
-favveg.forEach(function (veg): void {
-    console.log(veg)
-})
-
-interface Peep {
-    firstName: string,
-    age: number
-}
+favveg.forEach(function (veg) {
+    console.log(veg);
+});
 //Declaring array of "peeps"
-let peeps: Array<Peep> = [
+var peeps = [
     {
         firstName: "Rick",
         age: 18
@@ -68,24 +58,22 @@ let peeps: Array<Peep> = [
         age: 21
     }
 ];
-
 //Executing loop checkAge for array "peeps"
-
-for (let i = 0; i < peeps.length; i++) {
+for (var i = 0; i < peeps.length; i++) {
     checkAge(peeps[i].firstName, peeps[i].age);
-};
+}
+;
 //Alternative syntax for above for loop, using the singular of the plural array name to increment:
 // for (let peep of peeps){
 //     checkAge(peep.firstName, peep.age);
-
-function getLength(str: string) {
+function getLength(str) {
     return str.length; // return stops the function
-};
-
-let result: number = getLength("Hello World");
-
+}
+;
+var result = getLength("Hello World");
 if (result % 2 === 0) {
     console.log("The world is nice and even!");
-} else {
+}
+else {
     console.log("The world is an odd place!");
 }
